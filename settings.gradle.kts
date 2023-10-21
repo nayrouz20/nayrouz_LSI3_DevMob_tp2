aabdd9eee944884d624d1a0ecd637f438c5a58ac
nayrouz20 <nayrouzweslati2002@gmail.com>
2023-10-21 11:35:18 +0100

    Premier commit DM

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "weslati_nayrouz_TP1"
+include(":app")
